a7dea2ceba6b7f1a1500e771fb4572194555b334
Rajarshi Agarwal <72816727+King-Agashi@users.noreply.github.com>
2022-08-03 22:05:46 +0530

    Time: 22 ms (54.64%), Space: 5.9 MB (31.94%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        int i = 0, j = s.size()-1;
-        while (i <= j) if (s[i++] != s[j--]) return false;
-        return true;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int rev = 0;
+        while (rev < x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x == rev || x == rev / 10;
     }
 };
