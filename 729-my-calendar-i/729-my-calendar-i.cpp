66b48c00019e61df95f4b8d2968a143b7e73fcd2
Rajarshi Agarwal <72816727+King-Agashi@users.noreply.github.com>
2022-08-03 21:55:56 +0530

    Time: 106 ms (88.78%), Space: 38.9 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,13 +1,11 @@
 class MyCalendar {
-    set<pair<int, int>> books;
+    map<int, int> books;
 public:
     bool book(int s, int e) {
-        auto next = books.lower_bound({s, e});
-        if (next != books.end() && next->first < e)
-            return false;
-        if (next != books.begin() && s < (--next)->second)
-            return false;
-        books.insert({ s, e });
+        auto next = books.lower_bound(s);
+        if (next != books.end() && next->first < e) return false;
+        if (next != books.begin() && s < (--next)->second) return false;
+        books[s] = e;
         return true;
     }
 };
