7241e347805c03f8e022df621e6b04ee81d55482
Rajarshi Agarwal <72816727+King-Agashi@users.noreply.github.com>
2022-08-09 18:12:53 +0530

    Time: 35 ms (98.46%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& arr) {
-        int n = arr.size();
-        long long int modn = 1000000007;
-        vector<long long int> dp(n, 1);
         sort(arr.begin(), arr.end());
-        unordered_map<int, int> index;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(arr[i] % arr[j]){
+        long Ans = 0;
+        long mx = 1e9 + 7;
+        unordered_map<int, long> SubTree;
+        
+        for (int num: arr) {
+            long ways = 1;
+            double lim = sqrt(num);
+            for (int i=0, childA = arr[0]; childA<=lim; childA = arr[++i]) {
+                if (num % childA != 0)
                     continue;
-                }
-                int m = arr[i] / arr[j];
-                if(index.find(m) != index.end()){
-                    dp[i] += 1L*dp[j]*dp[index[m]];
-                    dp[i] %= modn;
-                }
+                int childB = num/childA;
+                if (SubTree.find(childB) != SubTree.end())
+                    ways = (ways + SubTree[childA]*SubTree[childB]*(childA == childB? 1:2) % mx);
             }
-            index[arr[i]] = i;
+            SubTree[num] = ways;
+            Ans = (Ans + ways) % mx;
         }
-        return accumulate(dp.begin(), dp.end(), 0L)%modn;
+        
+        return (int)Ans;
     }
 };
