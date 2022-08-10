9ae4e4cc3509bf2e03f7649db97996b6d651cc20
Rajarshi Agarwal <72816727+King-Agashi@users.noreply.github.com>
2022-08-10 19:47:52 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (65.33%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,49 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) { 
-        int i   =   0,
-            j   =   numbers.length-1;
-        while(i < j){
-            if(numbers[i] + numbers[j] == target)
-                return new int[] {i + 1, j + 1};
-            else if(numbers[i] + numbers[j] > target)
-                j--;
-            else
-                i++;
+    public int[] twoSum(final int[] a, final int target) {
+        int l = 0;
+        int r = a.length - 1;
+        while (l < r) {
+            final int sum = a[l] + a[r];
+            if (sum == target) {
+                return new int[]{l + 1, r + 1};
+            }
+            if (sum > target) {
+                r = leftBinarySearch(a, l + 1, r - 2, target - a[l]);
+            } else {
+                l = rightBinarySearch(a, l + 2, r - 1, target - a[r]);
+            }
         }
-        return new int[] {0,0};
+        throw new IllegalStateException("Impossible by the condition of this problem");
+    }
+
+    private int leftBinarySearch(final int[] a, int l, int r, final int key) {
+        l--;
+        r++;
+
+        while (r > l + 1) {
+            final int m = l + (r - l) / 2;
+            if (a[m] >= key) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+
+        return r;
+    }
+
+    private int rightBinarySearch(final int[] a, int l, int r, final int key) {
+        l--;
+        r++;
+
+        while (r > l + 1) {
+            final int m = l + (r - l) / 2;
+            if (a[m] <= key) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return l;
     }
 }
